86aceb28770e7eaa4803f71b66a4ca34b1f54555
ElFacuM <107945519+ElFacuM@users.noreply.github.com>
2022-09-08 01:21:49 -0300

    Update filename.js

--- JavaScript/filename.js
+++ JavaScript/filename.js
@@ -1,8 +1,3 @@
-<html>
-<head>
-<script type="text/javascript" src="filename.js" ></script>
-</head>
-<body>
-<input type="button" onclick="sayHello()" value="Say Hello" />
-</body>
-</html>
+function  saludar ( )  {
+alerta ( "Hola Mundo" )
+}

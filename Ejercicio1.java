09505a4cbd36c203ca3d6171a4b6e3132643c342
ElFacuM <107945519+ElFacuM@users.noreply.github.com>
2023-06-28 23:15:24 -0300

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,15 +1,1 @@
-public class EvaluacionesAlumno {
-    public static void main(String[] args) {
-        double evaluacion1 = 6.5;
-        double evaluacion2 = 7.8;
-        double evaluacion3 = 8.2;
 
-        double promedio = (evaluacion1 + evaluacion2 + evaluacion3) / 3;
-
-        if (promedio >= 7) {
-            System.out.println("Aprobado");
-        } else {
-            System.out.println("Reprobado");
-        }
-    }
-}
